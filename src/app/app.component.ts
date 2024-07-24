9c7da4196977e5747878f43144a299e5518cf66e
Henry Zhang <hzhang@freelancer.com>
2024-07-24 16:28:13 +1000

    Reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  selector: 'app-display-card',
+  standalone: true,
+  template: `
+    <div>
+      <h1>
+        <ng-content select="app-title"></ng-content>
+      </h1>
+      And some other stuff
+    </div>
+  `,
+})
+export class DisplayCardComponent {}
+
+@Component({
+  selector: 'app-title',
+  template: '<ng-content></ng-content>',
+  standalone: true,
+})
+export class TitleComponent {}
+
+@Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, TitleComponent, DisplayCardComponent],
+  template: `
+    <app-display-card>
+      <app-title i18n ngSkipHydration> Title! </app-title>
+    </app-display-card>
+  `,
 })
-export class AppComponent {
-  title = 'i18n-skiphydration-repro';
-}
+export class AppComponent {}
