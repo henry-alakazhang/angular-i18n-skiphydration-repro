c4e2c914db30cde7924a1ae686f698fd4b2c9c21
Henry Zhang <hzhang@freelancer.com>
2024-07-24 17:05:03 +1000

    Reproduction for all cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,22 +10,38 @@ import { RouterOutlet } from '@angular/router';
 export class TextComponent {}
 
 @Component({
+  selector: 'app-unhydrated-text',
+  template: '<ng-content></ng-content>',
+  standalone: true,
+})
+export class UnhydratedTextComponent {
+  @HostBinding('attr.ngSkipHydration') skipHydration = true;
+}
+
+@Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TextComponent],
+  imports: [CommonModule, RouterOutlet, TextComponent, UnhydratedTextComponent],
   template: `
     <!-- OK -->
     <div i18n>
-      <app-text ngSkipHydration> Title! </app-text>
+      <app-text ngSkipHydration> Text </app-text>
     </div>
 
-    <!-- OK -->
-    <app-text ngSkipHydration>
-      <div i18n>Title!</div>
-    </app-text>
+    <!-- ASSERTION ERROR: Expected root i18n node during hydration -->
+    <!-- <app-text i18n ngSkipHydration> Text </app-text> -->
+    <!-- <app-unhydrated-text i18n> Text </app-unhydrated-text> -->
 
-    <!-- not ok -->
-    <!-- <app-text i18n ngSkipHydration> Title! </app-text> -->
+    <!-- TypeError: rootNode is null -->
+    <!-- <app-text ngSkipHydration>
+      <div i18n>Text</div>
+    </app-text> -->
+
+    <!-- NG0502 During hydration Angular was unable to locate a node using the "firstChild" path,
+     starting from the <app-unhydrated-text ngskiphydration="true">…</app-unhydrated-text> node.-->
+    <!-- <app-unhydrated-text>
+      <div i18n>Title!</div>
+    </app-unhydrated-text> -->
   `,
 })
 export class AppComponent {}
